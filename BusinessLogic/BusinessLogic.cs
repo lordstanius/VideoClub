c2956b1eb15ed0b5cf2067eb81625d7244fea4eb
Mladen Stanisic <lordstanius@gmail.com>
2017-02-18 14:01:18 +0100

    Added another rule.

--- BusinessLogic/BusinessLogic.cs
+++ BusinessLogic/BusinessLogic.cs
@@ -9,6 +9,24 @@ namespace BusinessLogic
 
 		public bool AllowMultipleRentals { get; } = false;
 
+		public bool DoNotAllowRentalOfPreviouslyRentedMovie { get; } = false;
+
 		public int MovieNumberLimit { get; } = 4;
+
+		public int DuePeriod(int numOfMovies)
+		{
+			switch (numOfMovies)
+			{
+				case 1:
+					return 2;
+
+				case 2:
+				case 3:
+					return 4;
+
+				default:
+					return 6;
+			}
+		}
 	}
 }
